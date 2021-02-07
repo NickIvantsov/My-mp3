93f4f036ee8e1b5dba545402e17268f1990deabd
Nickolay-Ivantsov <ivantsov.nikolai.01@gmail.com>
2021-02-07 19:50:15 +0200

    добавил зависимость Glide, добавил отображение иконки альбома, исправил ошибку java.lang.IndexOutOfBoundsException на android 5.1 при добавлении одного элемента в список

--- My-mp3/app/src/main/java/com/gmail/ivantsov/nikolai/my_mp3/presentation/library/SongsAdapter.kt
+++ My-mp3/app/src/main/java/com/gmail/ivantsov/nikolai/my_mp3/presentation/library/SongsAdapter.kt
@@ -1,11 +1,14 @@
 package com.gmail.ivantsov.nikolai.my_mp3.presentation.library
 
+import android.content.ContentUris
 import android.content.Context
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.gmail.ivantsov.nikolai.core.domain.INT_NOT_INIT
 import com.gmail.ivantsov.nikolai.core.domain.Song
 import com.gmail.ivantsov.nikolai.my_mp3.R
@@ -14,26 +17,46 @@ import com.gmail.ivantsov.nikolai.my_mp3.R
 class SongsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     //region поля
 
+    companion object {
+        private const val EMPTY_VIEW = 77777
+        private const val URL_TO_ALBUM_ART = "content://media/external/audio/albumart"
+        private const val LIST_EMPTY = 0
+    }
+
     lateinit var itemClickListener: (Song) -> Unit
-    private val EMPTY_VIEW = 77777
     private val songs: MutableList<Song> = mutableListOf()
     private lateinit var context: Context
     private var songItemPosition = INT_NOT_INIT
     private var songItem: Song? = null
+    private val itemOnClickListener =
+        { _: View, holder: SongsViewHolder, position: Int, song: Song ->
+            songItemPosition = position
+            songItem = song
+            setItemColorActiveAndUpdate(holder, R.color.purple_200)
+            itemClickListener(song)
+        }
 
     //endregion
     //region интерфейсы
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         context = parent.context
         val inflater = LayoutInflater.from(context)
-        if (viewType == EMPTY_VIEW) {
-            val view: View =
-                inflater.inflate(R.layout.empty_songs_view, parent, false)
-            return EmptySongsViewHolder(view)
+        return if (viewType == EMPTY_VIEW) {
+            EmptySongsViewHolder(
+                getView(
+                    inflater,
+                    parent,
+                    R.layout.empty_songs_view
+                )
+            )
         } else {
-            val view: View =
-                inflater.inflate(R.layout.item_song, parent, false)
-            return SongsViewHolder(view)
+            SongsViewHolder(
+                getView(
+                    inflater,
+                    parent,
+                    R.layout.item_song
+                )
+            )
         }
 
     }
@@ -44,19 +67,20 @@ class SongsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
         } else {
             val song = songs[position]
             if (holder is SongsViewHolder) {
-                if (song.id == songItem?.id) {
-                    setItemColorActive(holder, R.color.purple_200)
-                } else {
-                    setItemColorNotActive(holder, R.color.black)
-                }
+                setItemColor(song, holder)
                 holder.binding.tvSongTitle.text = song.title
                 holder.binding.tvSongArtist.text = song.artistName
+                setAlbumArt(song, holder)
                 holder.binding.songItemContainer.setOnClickListener {
-                    songItemPosition = position
-                    songItem = song
-                    setItemColorActiveAndUpdate(holder, R.color.purple_200)
-                    itemClickListener(song)
+                    itemOnClickListener(
+                        it,
+                        holder,
+                        position,
+                        song
+                    )
                 }
+            } else {
+                //todo добавить исключение
             }
         }
 
@@ -71,11 +95,15 @@ class SongsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     fun addAll(songList: List<Song>) {
         songs.addAll(songList)
-        notifyItemInserted(songs.size - 1)
+        if (songList.size == 1)
+            notifyItemInserted(songs.size)
+        else {
+            notifyItemInserted(songs.size - 1)
+        }
     }
 
     override fun getItemViewType(position: Int): Int {
-        if (songs.size == 0) {
+        if (songs.size == LIST_EMPTY) {
             return EMPTY_VIEW
         }
         return super.getItemViewType(position)
@@ -83,6 +111,25 @@ class SongsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     //endregion
     //region реализация
+    private fun getView(
+        inflater: LayoutInflater,
+        parent: ViewGroup,
+        layoutId: Int
+    ): View {
+        return inflater.inflate(layoutId, parent, false)
+    }
+
+    private fun setItemColor(
+        song: Song,
+        holder: SongsViewHolder
+    ) {
+        if (song.id == songItem?.id) {
+            setItemColorActive(holder, R.color.purple_200)
+        } else {
+            setItemColorNotActive(holder, R.color.black)
+        }
+    }
+
     private fun setItemColorActiveAndUpdate(holder: SongsViewHolder, color: Int) {
         setItemColor(holder, color)
         notifyDataSetChanged()
@@ -98,6 +145,23 @@ class SongsAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     private fun setItemColor(holder: SongsViewHolder, color: Int) {
         holder.binding.tvSongTitle.setTextColor(ContextCompat.getColor(context, color))
+    }
+
+    private fun getAlbumArtUri(albumId: Long): Uri {
+        return ContentUris.withAppendedId(
+            Uri.parse(URL_TO_ALBUM_ART),
+            albumId
+        )
+    }
+
+    private fun setAlbumArt(
+        song: Song,
+        holder: SongsViewHolder
+    ) {
+        Glide.with(context)
+            .load(getAlbumArtUri(song.albumId))
+            .placeholder(R.drawable.ic_baseline_headset_24)
+            .into(holder.binding.imageView3);
     }
     //endregion
 }
