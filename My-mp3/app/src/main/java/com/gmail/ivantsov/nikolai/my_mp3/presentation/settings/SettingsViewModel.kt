4af511a5782816e09c5be8cd69f66b02a804e4d2
Nickolay-Ivantsov <ivantsov.nikolai.01@gmail.com>
2021-02-12 23:09:57 +0200

    добавил окно настроек

---
+++ My-mp3/app/src/main/java/com/gmail/ivantsov/nikolai/my_mp3/presentation/settings/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package com.gmail.ivantsov.nikolai.my_mp3.presentation.settings
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
