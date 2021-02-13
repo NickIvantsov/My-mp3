119c454c64abd829d5847c630e5052d0c176ea50
Nickolay-Ivantsov <ivantsov.nikolai.01@gmail.com>
2021-02-13 13:42:57 +0200

    добавил документацию к маперу

--- My-mp3/app/src/main/java/com/gmail/ivantsov/nikolai/my_mp3/framework/SongModelDataMapper.kt
+++ My-mp3/app/src/main/java/com/gmail/ivantsov/nikolai/my_mp3/framework/SongModelDataMapper.kt
@@ -3,8 +3,17 @@ package com.gmail.ivantsov.nikolai.my_mp3.framework
 import com.gmail.ivantsov.nikolai.core.domain.Song
 import com.gmail.ivantsov.nikolai.my_mp3.framework.model.SongModel
 
+/**
+ * Mapper class used to transform [Song] (in the domain layer) to [SongModel] in the
+ * presentation layer.
+ */
 class SongModelDataMapper {
-
+    /**
+     * Transform a [Song] into an [SongModel].
+     *
+     * @param song Object to be transformed.
+     * @return [SongModel].
+     */
     fun transform(song: Song) = SongModel(
         song.id,
         song.albumId,
@@ -16,6 +25,12 @@ class SongModelDataMapper {
         song.trackNumber
     )
 
+    /**
+     * Transform a [SongModel] into an [Song].
+     *
+     * @param song Object to be transformed.
+     * @return [Song].
+     */
     fun transform(song: SongModel) = Song(
         song.id,
         song.albumId,
@@ -27,6 +42,12 @@ class SongModelDataMapper {
         song.trackNumber
     )
 
+    /**
+     * Transform a Collection of [Song] into a Collection of [SongModel].
+     *
+     * @param songs Objects to be transformed.
+     * @return List of [SongModel].
+     */
     fun transform(songs: List<Song>): List<SongModel> {
         val resultSongsList = ArrayList<SongModel>()
         songs.forEach { song ->
